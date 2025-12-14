267b97f2ea45dd574e8c92087e3a874f67e083e4
Malte Herrmann <malteherrmann.mail@gmail.com>
2025-12-15 00:13:56 +0100

    add basic setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Battleship Commonware
+
+This repository contains an example implementation of Battleship
+built on top of Commonware primitives.
+It serves as an exercise to familiarize myself with the framework.
+The Battleship logic is using https://github.com/orhun/battleship-rs.
---
+++ src/main.rs
@@ -0,0 +1,104 @@
+/// This crate contains a simple implementation
+/// of the battleship Rust implementation by Orhun on GitHub:
+/// https://github.com/orhun/battleship-rs.
+///
+/// This is intended to be a hands-on exercise of integrating
+/// an existing codebase in Rust with the Commonware set of
+/// utilities.
+///
+/// # Implementation Steps
+///
+/// 1. Start a simple setup where commonware-p2p communicates between two nodes (two keys).
+///    They should send simple messages with an increasing counter variable
+///    for starters.
+///
+///    - This can use hardcoded information first but should be extended to take input arguments
+///      to define the key of the running instance.
+///    - Real projects mostly use clap for this so that can be added here, too.
+///
+/// 2. This can be extended to incorporate moves for the battleship game.
+///    As a first iteration, just shoot at increasing fields A1, B1, ... .
+///
+/// 3. Finally, e.g. using hashing operations, the players could play
+///    against each other automatically and one could watch in the terminal.
+///
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    time::Duration,
+};
+
+use bytes::Bytes;
+use commonware_cryptography::{PrivateKeyExt as _, Signer, ed25519};
+use commonware_p2p::{Manager, Sender, authenticated::discovery};
+use commonware_runtime::{Metrics, Runner, deterministic};
+use commonware_utils::NZU32;
+use governor::Quota;
+
+const ENDPOINT_1: u16 = 5670;
+const ENDPOINT_2: u16 = 5671;
+const MAX_MESSAGE_SIZE: u16 = 1024;
+
+fn main() {
+    // We're creating the private keys here that will communicate over the p2p
+    // connection, in order to exchange messages about the intended moves in the game.
+    let signer = ed25519::PrivateKey::from_seed(0);
+    let peer1 = ed25519::PrivateKey::from_seed(1).public_key();
+
+    let bootstrappers = vec![(
+        peer1.clone(),
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), ENDPOINT_2),
+    )];
+
+    // The p2p setup uses the local config for this proof-of-concept.
+    // TODO: does this have to be adjusted to check which key is running the binary?
+    // we'll have to support running two instances via CLI flags.
+    println!("setting up p2p config");
+    let p2p_config = discovery::Config::local(
+        signer.clone(),
+        b"BATTLESHIP_NAMESPACE",
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), ENDPOINT_1.into()),
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), ENDPOINT_1.into()),
+        bootstrappers,
+        MAX_MESSAGE_SIZE.into(),
+    );
+
+    // TODO: use tokio runner here - ideally abstract at some point.
+    let runner_config = deterministic::Config::new()
+        .with_seed(0)
+        .with_timeout(Some(Duration::from_secs(10)));
+
+    let executor = deterministic::Runner::new(runner_config);
+
+    executor.start(|context| async move {
+        let (mut network, mut oracle) =
+            discovery::Network::new(context.with_label("network"), p2p_config);
+
+        // We set the peers in the oracle (which in the context of commonware-p2p is
+        // the central entity to manage the list of connected peers).
+        let peers_index = 0;
+        oracle.update(peers_index, vec![peer1].into()).await;
+
+        // This registes the channel over which communication
+        // about the game state will be implemented.
+        //
+        // TODO: refactor to actor here and implement run and start methods.
+        let (mut sender, receiver) = network.register(0, Quota::per_second(NZU32!(1)), 1);
+
+        // TODO: do something with start handler here?
+        let network_handler = network.start();
+
+        // NOTE: This currently uses ::All but ::One would also work since there's
+        // only one peer.
+        //
+        // TODO: refactor this to be sent upon calling a different command, to control the game.
+        let message_bytes = Bytes::from_static(b"hello");
+        sender
+            .send(commonware_p2p::Recipients::All, message_bytes, false)
+            .await
+            .expect("failed to send via p2p");
+
+        network_handler.abort();
+    });
+
+    println!("done.");
+}
